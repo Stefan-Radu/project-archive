d20f524688d4dfe16c97fef80b41efe3f49d5df5
Stefan Radu <stefan270101@gmail.com>
2018-05-11 22:06:15 +0300

    learning opengl

--- C++/OpenGL/main.cpp
+++ C++/OpenGL/main.cpp
@@ -2,56 +2,40 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-using namespace std;
+int main () {
 
-int main() {
-
-  GLFWwindow* window;
-
-  if (not glfwInit()) {
-    cout << "Error\n";
+  GLFWwindow *window;
+  
+  if (not glfwInit ()) {
+    std::cerr << "glfw not initialized\n";
     return -1;
   }
-
-  window = glfwCreateWindow(600, 400, "wow", NULL, NULL);
-
+  
+  window = glfwCreateWindow (800, 600, "wow", nullptr, nullptr);
+ 
   if (not window) {
-    glfwTerminate();
-    cout << "Error\n";
+    glfwTerminate ();
+    std::cerr << "no window\n";
     return -1;
   }
 
-  glfwMakeContextCurrent(window);
-  if(not glewInit() == GLEW_OK) {
-    cout << "Error\n";
-    return -1;
-  }
-  
-  float positions[6] = {
-    -0.5f, 0.5f,
-    0.5f, 0.5f,
-    0.0f, -0.5f
-  };
+  glfwMakeContextCurrent (window);
 
-  unsigned int buffer;
-  glGenBuffers(1, &buffer); 
-  glBindBuffer(GL_ARRAY_BUFFER, buffer);
-  glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
-  
-  glEnableVertexAttribArray(0);
-  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+  while (not glfwWindowShouldClose (window)) {
 
-  while (not glfwWindowShouldClose(window)) {
+    glClear (GL_COLOR_BUFFER_BIT);
 
-    glClear(GL_COLOR_BUFFER_BIT);
-  
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glBegin (GL_TRIANGLES);
+    glVertex2d (-0.5f, 0.5f);
+    glVertex2d (0.5f, 0.5f);
+    glVertex2d (0.0f, -0.5f);
+    glEnd ();
 
-    glfwSwapBuffers(window);
+    glfwSwapBuffers (window);
 
-    glfwPollEvents();
+    glfwPollEvents ();
   }
 
-  glfwTerminate();
+  glfwTerminate ();
   return 0;
 }
